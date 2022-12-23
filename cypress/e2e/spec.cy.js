3f9a3f75398ba0b56f6430a2d2d16bbb25af723d
Markus Duft <markus.duft@ssi-schaefer.com>
2022-12-23 12:18:55 +0100

    add fixture test

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,20 @@
 /// <reference types="cypress" />
 describe('page', () => {
   it('works', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+    let name = 'example.json';
+
+    // THIS WORKS
+    cy.fixture(name, null).then((x) => cy.log(x));
+
+    cy.origin('https://google.com', { args: name }, (name) => {
+      // THIS WORKS
+      // cy.fixture(name).then((x) => cy.log(x));
+
+      // THIS DOES NOT WORK (only inside cy.origin, see above)
+      cy.fixture(name, null).then((x) => cy.log(x));
+
+      // THIS DOES NOT WORK (saw it like that in cypress docs, but that would not even fix the API...?!)
+      // cy.fixture(name, { encoding: null }).then((x) => cy.log(x));
+    });
+  });
+});
