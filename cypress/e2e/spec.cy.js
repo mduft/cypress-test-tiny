3809ede66142dfedc1e0d23c9d4fb22b83b5bc94
Markus Duft <markus.duft@ssi-schaefer.com>
2022-12-23 10:38:18 +0100

    Add selectFile test

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,10 @@
 /// <reference types="cypress" />
 describe('page', () => {
   it('works', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+    cy.origin('https://www.w3schools.com', () => {
+      cy.visit('/howto/howto_html_file_upload_button.asp');
+
+      cy.get('input[type=file]').selectFile({ contents: 'cypress/fixtures/example.json' }, { force: true });
+    });
+  });
+});
